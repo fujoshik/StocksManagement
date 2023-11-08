2d49cc55b47441554d8425a4fe5695458e87b672
nikolaiM12 <nikolaimashkov987@gmail.com>
2023-11-08 18:47:37 +0200

    Added Http Requests & Implementation on ExecuteDeal

---
+++ Settlement MS/Settlement.Domain/DTOs/Transaction/TransactionRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Settlement.Domain.DTOs.Transaction
+{
+    internal class TransactionRequestDto
+    {
+    }
+}
