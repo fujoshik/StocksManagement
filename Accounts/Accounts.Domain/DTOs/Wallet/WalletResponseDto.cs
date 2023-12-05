d49dfd724a014f31a553af9e714efcfe170ad67e
RadaIlieva <radaivajlovailieva@gmail.com>
2023-12-05 08:54:10 +0200

    CurrentYield

--- Accounts/Accounts.Domain/DTOs/Wallet/WalletResponseDto.cs
+++ Accounts/Accounts.Domain/DTOs/Wallet/WalletResponseDto.cs
@@ -7,6 +7,5 @@ namespace Accounts.Domain.DTOs.Wallet
         public decimal InitialBalance { get; set; }
         public decimal CurrentBalance { get; set; }
         public CurrencyCode CurrencyCode { get; set; }
-        public object UserData { get; set; }
     }
 }
