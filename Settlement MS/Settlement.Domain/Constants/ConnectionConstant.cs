57718bea2c677601e53d9fd896dd1bee36e2539a
RadaIlieva <radaivajlovailieva@gmail.com>
2023-12-19 13:59:27 +0200

    conection

--- Settlement MS/Settlement.Domain/Constants/ConnectionConstant.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Settlement.Domain.Constants
-{
-    public class ConnectionConstant
-    {
-       public const string connectionString = "Server=RADA\\SQLEXPRESS01;Database=StocksDB;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=True;";
-    }
-}
